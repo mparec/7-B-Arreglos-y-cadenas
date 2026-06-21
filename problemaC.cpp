90f6b87b8a12c52465b0d44a33a2da80c8f87165
mparec <mparec@unjbg.edu.pe>
2026-06-21 12:47:50 -0500

    Ejercicio C: Establecer el arreglo fuera de la funcion principal

--- problemaC.cpp
+++ problemaC.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int A[1000]; // Todos los elementos se inicializan automaticamente en 0
+
 int main(){
     int P, n, F;
 
-    int A[1000];
-
     cin >> P >> n; // Cantidad de profesores y de alumnos
-
-    for (int i = 0; i < P; i++) {
-        A[i] = 0; // Establecer valor 0 en lugar de valor vacio
-    }
 
     for (int i = 0; i < n; i++) {
         cin >> F;
