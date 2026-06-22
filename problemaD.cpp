299cc7f32efd2b488e06c7d3fc6e690c73fa4a54
mparec <mparec@unjbg.edu.pe>
2026-06-21 23:57:16 -0500

    Ejercicio D: Primer intento de Canicas

---
+++ problemaD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int A[1000000];
+
+int main(){
+    int n, c, v; // cantidad de vasos, cantidad de canicas y canicas en cada vaso
+
+    cin >> n >> c;
+
+    for (int i = 0; i < n; i++) {
+        A[i] = 0; // Rellenar valores vacios con el valor 0
+    }
+
+    for (int i = 0; i < c; i++) {
+        cin >> v; // Establecer cantidad "c" de canicas en cada vaso
+        A[v - 1]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << endl;
+    }
+
+    return 0;
+}
